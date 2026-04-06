8d7d1faebc5d1d00af2776784dc79309a447adf0
Ivan E. <iamioann@yandex.ru>
2026-04-06 17:11:44 +0300

    chore: initial commit

---
+++ src/main/kotlin/efr/iv/igr/dobr_kvtest/DobrKvTestApplication.kt
@@ -0,0 +1,11 @@
+package efr.iv.igr.dobr_kvtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DobrKvTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<DobrKvTestApplication>(*args)
+}
---
+++ src/test/kotlin/efr/iv/igr/dobr_kvtest/DobrKvTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package efr.iv.igr.dobr_kvtest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DobrKvTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
